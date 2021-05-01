9bbeae9dcc1326eb0c3bd56876cdd6f066d31c45
Nikhil <43917504+skidi3@users.noreply.github.com>
2021-05-01 15:24:40 +0530

    Create rotate-array.cpp
    leetcode-189

---
+++ Array/rotate-array.cpp
@@ -0,0 +1,26 @@
+/* Question: https://leetcode.com/problems/rotate-array/
+
+Given an array, rotate the array to the right by k steps, 
+where k is non-negative.
+
+Solution: */ 
+
+class Solution {
+public:
+    void reverse(vector<int>& nums, int start, int end) {
+        while (start < end) {
+          int temp = nums[start];
+          nums[start] = nums[end];
+          nums[end] = temp;
+          start++;
+          end--;
+        }
+    }
+    void rotate(vector<int>& nums, int k) {
+        k = k % nums.size();
+        reverse(nums, 0, nums.size() - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.size() - 1); 
+    }
+    
+};
