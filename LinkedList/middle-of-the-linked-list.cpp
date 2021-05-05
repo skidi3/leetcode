621dff778cbbcad45245d7a3de234570696d483a
Samarth Gupta <43917503+samarthgupta0309@users.noreply.github.com>
2021-05-05 19:33:05 +0530

    Create middle-of-the-linked-list.cpp
    Solution of Middle of the Linked List in cpp

---
+++ LinkedList/middle-of-the-linked-list.cpp
@@ -0,0 +1,29 @@
+/**
+  Question : https://leetcode.com/problems/middle-of-the-linked-list/
+  Middle of the Linked List
+  
+  Given a non-empty, singly linked list with head node head, return a middle node of linked list.
+  If there are two middle nodes, return the second middle node.
+ 
+ Solution :
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* slow=head; 
+        ListNode* fast=head;
+        while(fast!=NULL && fast->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+};
